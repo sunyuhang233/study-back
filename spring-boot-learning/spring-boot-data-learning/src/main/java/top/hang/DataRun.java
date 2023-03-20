8c817f5d4fd8d1848db4444e8a897a981570f4e4
‘hang’ <2283582431@qq.com>
2023-03-20 13:24:42 +0800

    '初始化数据访问'

---
+++ spring-boot-learning/spring-boot-data-learning/src/main/java/top/hang/DataRun.java
@@ -0,0 +1,17 @@
+package top.hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ahang
+ * @version 1.0
+ * @description TODO
+ * @date 2023/3/20 13:21
+ */
+@SpringBootApplication
+public class DataRun {
+    public static void main(String[] args) {
+        SpringApplication.run(DataRun.class, args);
+    }
+}
---
+++ spring-boot-learning/spring-boot-data-learning/src/test/java/top/hang/springbootdatalearning/SpringBootDataLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package top.hang.springbootdatalearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
